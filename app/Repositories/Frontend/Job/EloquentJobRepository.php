ff26404feab08f28dc22767e2aca57320dbe97a7
jishcem <jishcem@gmail.com>
2016-01-15 15:45:13 +0530

    jobs search completed

--- app/Repositories/Frontend/Job/EloquentJobRepository.php
+++ app/Repositories/Frontend/Job/EloquentJobRepository.php
@@ -1,6 +1,9 @@
 <?php namespace App\Repositories\Frontend\Job;
 
 use App\Models\Job\Job;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
 
 /**
  * Class EloquentUserRepository
@@ -8,11 +11,119 @@ use App\Models\Job\Job;
  */
 class EloquentJobRepository {
 
-    public function getJobsPaginated($per_page, $order_by = 'jobs.id', $sort = 'asc') {
-        return Job::where('status', true)
-            ->where('published', true)
+    public function getJobsPaginated(Request $request, $per_page, $order_by = 'jobs.created_at', $sort = 'desc') {
+
+        $searchObj = new Job();
+
+        // First the joins
+        if ( $request->get('categories') ) {
+            $searchObj = $searchObj->join('category_preferences_jobs', 'category_preferences_jobs.job_id', '=', 'jobs.id');
+        }
+        if ( $request->get('skills') ) {
+            $searchObj = $searchObj->join('job_skills', 'job_skills.job_id', '=', 'jobs.id');
+        }
+
+        // then the where conditions
+        if ( $request->get('level') ) {
+            $searchObj = $searchObj->where('jobs.level', $request->get('level'));
+        }
+        if ( $request->get('categories') ) {
+            $searchObj = $searchObj->whereIn('category_preferences_jobs.job_category_id', $request->get('categories'));
+        }
+        if ( $request->get('skills') ) {
+            $searchObj = $searchObj->whereIn('job_skills.skill_id', $request->get('skills'));
+        }
+        if ( $request->get('country_id') ) {
+            $searchObj = $searchObj->where('jobs.country_id', $request->get('country_id'));
+        }
+        if ( $request->get('state_id') ) {
+            $searchObj = $searchObj->where('jobs.state_id', $request->get('state_id'));
+        }
+
+        $searchObj = $searchObj
+            ->where('jobs.status', true)
+            ->where('jobs.published', true)
+            ->groupBy('jobs.id');
+
+        $jobs = $searchObj
+            ->with('categories', 'skills', 'company')
             ->orderBy($order_by, $sort)
-            ->paginate($per_page);
+            ->skip((Paginator::resolveCurrentPage()-1)*($per_page))
+            ->take($per_page)
+            ->select([
+                'jobs.id',
+                'jobs.company_id',
+                'jobs.title',
+                'jobs.title_url_slug',
+                'jobs.level',
+                'jobs.country_id',
+                'jobs.state_id',
+                'jobs.likes',
+                'jobs.created_at'
+            ])
+            ->get();
+
+        $paginator = $this->getJobsPaginator($request, $jobs, $per_page);
+
+        return [
+            'jobs'              => $jobs,
+            'paginator'         => $paginator
+        ];
+
+    }
+
+    public function getJobsPaginator(Request $request, $jobs, $perPage){
+
+        $curPage = Paginator::resolveCurrentPage();
+
+        $searchObj = new Job();
+
+        // First the joins
+        if ( $request->get('categories') ) {
+            $searchObj = $searchObj->join('category_preferences_jobs', 'category_preferences_jobs.job_id', '=', 'jobs.id');
+        }
+        if ( $request->get('skills') ) {
+            $searchObj = $searchObj->join('job_skills', 'job_skills.job_id', '=', 'jobs.id');
+        }
+
+        // then the where conditions
+        if ( $request->get('level') ) {
+            $searchObj = $searchObj->where('jobs.level', $request->get('level'));
+        }
+        if ( $request->get('categories') ) {
+            $searchObj = $searchObj->whereIn('category_preferences_jobs.job_category_id', $request->get('categories'));
+        }
+        if ( $request->get('skills') ) {
+            $searchObj = $searchObj->whereIn('job_skills.skill_id', $request->get('skills'));
+        }
+        if ( $request->get('country_id') ) {
+            $searchObj = $searchObj->where('jobs.country_id', $request->get('country_id'));
+        }
+        if ( $request->get('state_id') ) {
+            $searchObj = $searchObj->where('jobs.state_id', $request->get('state_id'));
+        }
+
+        $searchObj = $searchObj
+            ->where('jobs.status', true)
+            ->where('jobs.published', true)
+            ->groupBy('jobs.id');
+
+        $jobs_count = $searchObj->select([
+            \DB::raw('jobs.id AS job_id'), \DB::raw('count(*) as total')
+        ]);
+
+        $jobs_count = \DB::table( \DB::raw("({$jobs_count->toSql()}) as sub") )
+            ->mergeBindings($jobs_count->getQuery()) // you need to get underlying Query Builder
+            ->count();
+
+        $paginator = new LengthAwarePaginator(
+            $jobs, $jobs_count, $perPage, $curPage,
+            ['path' => Paginator::resolveCurrentPath()]
+        );
+
+        $paginator->appends($request->except(['page']));
+
+        return $paginator;
     }
 
 }

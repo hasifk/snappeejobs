61f70c4c7e3e2435ececc7ffbf3b797314d2fd13
jishcem <jishcem@gmail.com>
2016-03-12 10:47:29 +0530

    Group chat page

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -199,6 +199,45 @@
 
 
         <div class="col-md-6">
+
+            <div id="carousel-example-captions" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-captions" data-slide-to="0" class=""></li>
+                    <li data-target="#carousel-example-captions" data-slide-to="1" class="active"></li>
+                    <li data-target="#carousel-example-captions" data-slide-to="2" class=""></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <div class="item">
+
+                        <div class="carousel-caption"><h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+
+                        </div>
+                    </div>
+                    <div class="item active">
+                        <div class="carousel-caption"><h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="carousel-caption"><h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p></div>
+                    </div>
+                </div>
+                <a class="left carousel-control" href="#carousel-example-captions" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> <span class="sr-only">Previous</span>
+                </a> <a class="right carousel-control" href="#carousel-example-captions" role="button"
+                        data-slide="next"> <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span> </a></div>
+
             <div id="employer-scrolling-info" class="carousel slide" data-ride="carousel">
                 <ol class="carousel-indicators">
                     <li data-target="#employer-scrolling-info" data-slide-to="0" class=""></li>
@@ -234,6 +273,8 @@
             socket.on('employer_staff.{{ auth()->user()->id }}:employer_notifications', function(data){
                 $('.employer-notitications .employer-notitications-list').append('<li class="item"><div class="product-info" style="margin-left: 25px;"><a class="product-title" href="#">'+ data.eventDetails.job_title +'<span class="label label-warning pull-right">'+ data.eventDetails.notification_type_text +'</span></a><span class="product-description">'+ data.eventDetails.notification_type_text +'</span>'+ data.eventDetails.created_by +'</div></li>');
             });
+
+            $("#employer-scrolling-info").carousel();
 
         });
 

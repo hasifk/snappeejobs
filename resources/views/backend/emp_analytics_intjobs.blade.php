3da2715256040c064e22920d04cef8092ed0852a
Ajay <Ajay>
2016-03-16 15:29:29 +0530

    New features on interested jobs-liked jobseeker's info

--- resources/views/backend/emp_analytics_intjobs.blade.php
+++ resources/views/backend/emp_analytics_intjobs.blade.php
@@ -19,73 +19,23 @@
 
 
         @roles(['Employer', 'Employer Staff'])
+        @if(count($interested_jobs)>0)
            <h3>Interested Jobs</h3>
-
+        <ul class="list-group">
         @foreach($interested_jobs as $job)
             @if($job->likes>0)
-            <div class="col-md-4" id="job_list">
-                <div class="job-card">
-                    <div class="row">
-                        <div class="col-md-12 heading">
-                            <a href="{{ route('jobs.view' , [ $job->company->url_slug , $job->title_url_slug ] ) }}">{{ $job->title }}</a>
-                        </div>
-                        <div class="col-md-12">
+                <li class="list-group-item">
+                    <a href="{{ route('jobs.view' , [ $job->company->url_slug , $job->title_url_slug ] ) }}">{{ $job->title }}</a>
+                    Liked by:<a href="{{ route('jobseeker.show' , [ $job->id ] ) }}">{{ $job->name }}</a>
+                </li>
 
-                        </div>
-                        <div class="col-md-12 sub-heading">
-                            <a href="{{ route('companies.view', ['slug' => $job->company->url_slug]) }}">
-                                {{ str_studly($job->company->title) }}
-                            </a>
-                            <br>
-                            <span class="label label-danger">
-                                <a href="{{ route('jobs.search', ['level' => $job->level]) }}">
-                                    {{ str_studly($job->level) }}
-                                </a>
-                            </span>
-                        </div>
-                        <hr>
-                        <div class="col-md-12">
-                            @foreach($job->categories as $category)
-                                <div class="label label-info">
-                                    <a href="{{ route('jobs.search', ['category' => $category->id]) }}">
-                                        {{ $category->name }}
-                                    </a>
-                                </div>
-                            @endforeach
-                        </div>
-                        <hr>
-                        <div class="col-md-12">
-                            @foreach($job->skills as $skill)
-                                <div class="label label-success">
-                                    <a href="{{ route('jobs.search', ['skill' => $skill->id]) }}">
-                                        {{ $skill->name }}
-                                    </a>
-                                </div>
-                            @endforeach
-                        </div>
-                        <div class="col-md-12">
-                            <div for="" class="label label-info">
-                                <a href="{{ route('jobs.search', ['country' => $job->country_id]) }}">
-                                    {{ $job->country->name }}
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                            <div for="" class="label label-info">
-                                <a href="{{ route('jobs.search', ['state' => $job->state_id]) }}">
-                                    {{ $job->state->name }}
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
             @endif
         @endforeach
-
+       </ul>
         <div class="col-md-12 center-block">
-            {!! $paginator->render() !!}
+            {!! $interested_jobs->render() !!}
         </div>
+        @endif
         @endauth
 
     </div>

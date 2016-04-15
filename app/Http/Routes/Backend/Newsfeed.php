583adc204be3aa45ccd1ac0a4ff38129e3d2d25c
Ajay <Ajay>
2016-04-15 15:34:18 +0530

    Console Custom Commands

--- app/Http/Routes/Backend/Newsfeed.php
+++ app/Http/Routes/Backend/Newsfeed.php
@@ -4,7 +4,7 @@
     $router->group(['namespace' => 'Newsfeed'], function() use ($router) {
         get('newsfeeds', 'AdminNewsfeedController@showNewsfeeds')->name('backend.admin.newsfeeds');
         get('newsfeed', 'AdminNewsfeedController@createNewsfeed')->name('backend.admin.newsfeed');
-        post('newsfeedsave', 'AdminNewsfeedController@SaveNewsfeed')->name('backend.admin.newsfeedsave');
+        post('newsfeedsave', 'AdminNewsfeedController@saveNewsfeed')->name('backend.admin.newsfeedsave');
         get('newsfeed/{id}', 'AdminNewsfeedController@showNewsfeed')->name('backend.admin.newsfeedshow');
         get('newsfeed/{id}/edit', 'AdminNewsfeedController@EditNewsfeed')->name('backend.admin.newsfeed.edit');
         get('newsfeed/destroy/{id}', 'AdminNewsfeedController@DeleteNewsfeed')->name('backend.admin.newsfeed.destroy');

39d8acb12112e3142158cecaa220bc61cfc6c1c6
Ajay <Ajay>
2016-04-21 11:09:28 +0530

    Fix migration.

--- database/migrations/2016_04_20_112107_create_job_application_status_history_table.php
+++ database/migrations/2016_04_20_112107_create_job_application_status_history_table.php
@@ -14,8 +14,8 @@ class CreateJobApplicationStatusHistoryTable extends Migration
     {
         Schema::create('job_application_status_history', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('job_application_id')->unisnged();
-            $table->integer('job_application_status_company_id')->unisnged();
+            $table->integer('job_application_id')->unsigned();
+            $table->integer('job_application_status_company_id')->unsigned();
             $table->timestamps();
 
             $table->foreign('job_application_id')
@@ -23,7 +23,7 @@ class CreateJobApplicationStatusHistoryTable extends Migration
                 ->on('job_applications')
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
-            $table->foreign('job_application_status_company_id')
+            $table->foreign('job_application_status_company_id','jb_apst_cmp')
                 ->references('id')
                 ->on('job_application_status_company')
                 ->onUpdate('cascade')
@@ -40,7 +40,7 @@ class CreateJobApplicationStatusHistoryTable extends Migration
     {
         Schema::table('job_application_status_history', function (Blueprint $table) {
             $table->dropForeign('job_application_status_history_job_application_id_foreign');
-            $table->dropForeign('job_application_status_history_job_application_status_company_id_foreign');
+            $table->dropForeign('job_application_status_history_jb_apst_cmp_foreign');
         });
 
         Schema::drop('job_application_status_history');

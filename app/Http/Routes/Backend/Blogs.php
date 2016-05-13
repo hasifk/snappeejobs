aa4d69f740b49ea4b9a8738a6d405a9179d767ec
jishcem <jishcem@gmail.com>
2016-05-13 14:25:36 +0530

    fix route links

--- app/Http/Routes/Backend/Blogs.php
+++ app/Http/Routes/Backend/Blogs.php
@@ -10,10 +10,10 @@ $router->group(['namespace' => 'Blogs'], function () use ($router)
      */
     $router->group([], function ()
     {
-        get('Blogs/manageblogs', 'BlogsController@index')->name('Blogs');
-        get('Blogs/createblogs', 'BlogsController@createBlog')->name('Blogs.create');
-        get('Blogs/editblogs/{id}', 'BlogsController@editBlog')->name('Blogs.edit');
-        post('Blogs/saveblogs', 'BlogsController@saveBlog')->name('frontend.blogsave');
+        get('blogs/manageblogs', 'BlogsController@index')->name('Blogs');
+        get('blogs/createblogs', 'BlogsController@createBlog')->name('Blogs.create');
+        get('blogs/editblogs/{id}', 'BlogsController@editBlog')->name('Blogs.edit');
+        post('blogs/saveblogs', 'BlogsController@saveBlog')->name('frontend.blogsave');
         get('get-subcats/{id}', function($id){
             $sub_cats = DB::table('blog_sub_cats')->where('blog_category_id', $id)->select(['id', 'name'])->get();
             return response()->json($sub_cats);

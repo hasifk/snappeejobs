696d8ae24b0de614b9a85ec0f3a1c4d6e700e494
Ajay <Ajay>
2016-05-25 10:24:35 +0530

    create blog fix.

--- app/Http/Routes/Backend/Blogs.php
+++ app/Http/Routes/Backend/Blogs.php
@@ -14,7 +14,7 @@ $router->group(['namespace' => 'Blogs'], function () use ($router)
         get('blogs/createblogs', 'BlogsController@createBlog')->name('Blogs.create');
         get('blogs/editblogs/{id}', 'BlogsController@editBlog')->name('Blogs.edit');
         post('blogs/saveblogs', 'BlogsController@saveBlog')->name('frontend.blogsave');
-        get('get-subcats/{id}', function($id){
+        get('blogs/getsubcats/{id}', function($id){
             $sub_cats = DB::table('blog_sub_cats')->where('blog_category_id', $id)->select(['id', 'name'])->get();
             return response()->json($sub_cats);
         });

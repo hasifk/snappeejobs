d9e8df8f1c30432fa92da978cfa4c78c398fd8a2
Ajay <Ajay>
2016-06-02 17:30:24 +0530

    testing

--- tests/acceptance/CpanelAdminCept.php
+++ tests/acceptance/CpanelAdminCept.php
@@ -28,69 +28,10 @@ $I->click('#newnewsfeed');
 $I->see('News Feed');
 $I->amOnPage('/admin/cms');
 $I->see('CMS Management');
-$I->click('#cms_action');
+$I->amOnPage('/admin/cms_create');
 $I->see('CMS Management');
-$I->click('#cms_create');
-/*$I->click('#cms_all');
-$I->click('#cms_articles');
-$I->click('#cms_blogs');*/
+$I->amOnPage('/admin/cms');
+$I->amOnPage('/admin/cms/lists/article');
 $I->see('CMS Management');
 
-
-
-/*
-$I1 = new AcceptanceTester($scenario);
-$I1->wantTo('Ensure Profile Update works');
-$I1->amOnPage('/profile/edit');
-$I1->see('Update');
-
-$I2 = new AcceptanceTester($scenario);
-$I2->wantTo('Ensure Change Password works');
-$I2->amOnPage('/auth/password/change');
-$I2->see('Change');
-
-$I3 = new AcceptanceTester($scenario);
-$I3->wantTo('Ensure favourites page works');
-$I3->amOnPage('/profile/favourites');
-$I3->see('Favourites');
-
-$I4 = new AcceptanceTester($scenario);
-$I4->wantTo('Ensure admin dashboard page works');
-$I4->amOnPage('/admin/dashboard');
-$I4->see('Employer Count');
-
-$I5 = new AcceptanceTester($scenario);
-$I5->wantTo('Ensure admin access management page works');
-$I5->amOnPage('/admin/access/users');
-$I5->see('User Management');
-
-$I6 = new AcceptanceTester($scenario);
-$I6->wantTo('Ensure admin subscription management page works');
-$I6->amOnPage('/admin/subscription');
-$I6->see('subscription');
-
-$I7 = new AcceptanceTester($scenario);
-$I7->wantTo('Ensure admin company management page works');
-$I7->amOnPage('/admin/companies');
-$I7->see('Company Management');
-
-$I8 = new AcceptanceTester($scenario);
-$I8->wantTo('Ensure admin Jobseekers page works');
-$I8->amOnPage('/admin/jobseekers');
-$I8->see('JobSeekers');
-
-$I9 = new AcceptanceTester($scenario);
-$I9->wantTo('Ensure admin NewsFeeds page works');
-$I9->amOnPage('/admin/newsfeeds');
-$I9->click('#newnewsfeed');
-$I9->see('News Feed');*/
-
-/*$I10 = new AcceptanceTester($scenario);
-$I10->wantTo('Ensure admin Cms page works');
-$I10->amOnPage('/admin/cms');
-$I10->click('#cms_create');
-$I10->click('#cms_all');
-$I10->click('#cms_articles');
-$I10->click('#cms_blogs');
-$I10->see('CMS Management');*/
 
